8b6a7e699c2c398dc006d3584c7a489629394849
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:54:12 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[name,setName]=useState('');
-  const[greetinsg,setGreetings]=useState('');
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  const handleClick = () => {
+    setDisplayText(inputText);
+  }
+  if(inputText.trim()!==''){
+    setDisplayText('Hello,${inputText}')
+  }
 
   return (
-   
+   <div className='Greet'>
+    <h1><b>Hey!!! Greetings</b></h1>
+    <form onSubmit={handleClick} data-testing='form'>
+      <label data-testid='name'>Enter Your </label>
+    </form>
+   </div>
   );
 }
 
